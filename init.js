6480db1a87cc85f6f896be1322d5578f457a99a7
Victor <vicurd96@gmail.com>
2018-05-28 15:42:00 -0400

    Added scroll-to-top button and fixed some things.

--- init.js
+++ init.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.tooltipped').tooltip();
     $('.materialboxed').materialbox();
-  });
+    $('.fixed-action-btn').floatingActionButton({
+      hoverEnable:false
+    });
+    $(function() {
+      $(window).scroll(function() {
+          if($(this).scrollTop() != 0) {
+              $('#backto').fadeIn();
+          } else {
+              $('#backto').fadeOut();
+          }
+      });
+
+      $("#backto").click(function(event){
+        event.preventDefault();
+        $("body").animate({"scrollTop": "0px"}, 1000);
+      });
+    });
+});
