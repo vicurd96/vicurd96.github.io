3d34b6491bd3d66d50ecda5cdf38222ae32cf33f
vicurd96 <vicurd96@gmail.com>
2018-07-22 20:26:48 -0400

    Minor fix bug with carousel and ids.

--- init.js
+++ init.js
@@ -6,15 +6,45 @@ $(document).ready(function(){
       fullWidth: true,
       indicators: true
     });
-    setInterval(function() {
-      $('#skills').carousel('next');
-    }, 5000); // every 2 seconds*/
-    setInterval(function() {
-      $('#achievements').carousel('next');
-    }, 7000); // every 2 seconds*/
+
+    var skillsId = setInterval(function() {
+      if(!($("#skills").get(0).M_Carousel.pressed) && !($("#skills").get(0).M_Carousel.dragged)){
+        $('#skills').carousel('next');
+      }
+    }, 7000);
+
+    $("#skills").on('click',function() {
+      clearInterval(skillsId);
+      setTimeout(function() {
+        setInterval(function() {
+          if(!($("#skills").get(0).M_Carousel.pressed) && !($("#skills").get(0).M_Carousel.dragged)){
+            $('#skills').carousel('next');
+          }
+        }, 7000);
+      }, 10000);
+    });
+
+    var achievementsId = setInterval(function() {
+      if(!($("#achievements").get(0).M_Carousel.pressed) && !($("#achievements").get(0).M_Carousel.dragged)){
+        $('#achievements').carousel('next');
+      }
+    }, 5000);
+
+    $("#achievements").on('click',function() {
+      clearInterval(achievementsId);
+      setTimeout(function() {
+        setInterval(function() {
+          if(!($("#achievements").get(0).M_Carousel.pressed) && !($("#achievements").get(0).M_Carousel.dragged)){
+            $('#achievements').carousel('next');
+          }
+        }, 5000);
+      }, 10000);
+    });
+
     $('.fixed-action-btn').floatingActionButton({
       hoverEnable:false
     });
+
     $(function() {
       $(window).scroll(function() {
           if($(this).scrollTop() != 0) {
@@ -29,17 +59,4 @@ $(document).ready(function(){
         $("body").animate({"scrollTop": "0px"}, 1000);
       });
     });
-    //$("#page2").hide();
-    /*$("#next").click(function(){
-        $("#page1").hide(500);
-        $("#page2").show(1500);
-        $("#back").attr("class", "waves-effect");
-        $("#next").attr("class", "disabled");
-    });
-    $("#back").click(function(){
-        $("#page2").hide(500);
-        $("#page1").show(1500);
-        $("#back").attr("class", "disabled");
-        $("#next").attr("class", "waves-effect");
-    });*/
 });
